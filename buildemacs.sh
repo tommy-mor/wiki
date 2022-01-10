4bd8305f42b893f8434fc3f97ec82a80023f4df9
tommy <thmorriss@gmail.com>
2022-01-10 13:28:33 -0800

    emacs config last line

--- buildemacs.sh
+++ buildemacs.sh
@@ -1,9 +1,10 @@
 #https://www.emacswiki.org/emacs/GccEmacs
 
-sudo apt install autoconf make gcc texinfo libgtk-3-dev libxpm-dev libjpeg-dev libgif-dev libtiff5-dev libgnutls28-dev libncurses5-dev
+sudo apt install autoconf make gcc texinfo libgtk-3-dev libxpm-dev libjpeg-dev libgif-dev libtiff5-dev libgnutls28-dev libncurses5-dev libgccjit-9-dev
 
 git clone git://git.savannah.gnu.org/emacs.git
 cd emacs
 ./autogen.sh
 ./configure --with-native-compilation
 make -j$(nproc)
+sudo make install
